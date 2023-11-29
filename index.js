ce0ca16973d1c3bd9b7ec277f7f779db63d7fb28
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-11-29 11:18:03 +0600

    Get operation working but haven't used TanStack yet

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+
+// MongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.shklq4p.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const apartmentsCollection = client.db("SkyNest").collection("apartments");
+
+    app.get('/apartments', async(req,res) => {
+      const result = await apartmentsCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/',(req,res) => {
     res.send('SkyNest flying');
