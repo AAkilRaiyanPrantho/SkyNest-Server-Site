1f0daf603d4436ffb8637bd1e9b9f269185e8441
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-11-30 23:49:07 +0600

    Vercel Deployed

--- index.js
+++ index.js
@@ -82,8 +82,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
